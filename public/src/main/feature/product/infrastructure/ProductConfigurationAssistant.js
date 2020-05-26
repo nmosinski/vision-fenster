f8d39feda6392b91b21bc96fda4d220836aef6c4
nmosinski <norbert.mosinski@fh-muenster.de>
2020-05-26 10:58:55 +0200

    Renamed configuration services to configuration assistants

---
+++ public/src/main/feature/product/infrastructure/ProductConfigurationAssistant.js
@@ -0,0 +1,14 @@
+const PATH = "public/src/main/feature/product/infrastructure/ProductConfigurationAssistant.js";
+
+export default class ProductConfigurationAssistant
+{
+	constructor()
+	{
+
+	}
+
+	getValidOptionVariantIds(productType)
+	{
+		
+	}
+}
---
+++ public/src/main/feature/product/infrastructure/window/WindowProductConfigurationAssistant.js
@@ -0,0 +1,11 @@
+const PATH = "public/src/main/feature/product/infrastructure/WindowProductConfigurationAssistant.js";
+
+import ProductConfigurationAssistant from "public/src/main/feature/product/infrastructure/ProductConfigurationAssistant.js"
+
+export default class WindowProductConfigurationAssistant extends ProductConfigurationAssistant
+{
+	constructor()
+	{
+		super();
+	}
+}
