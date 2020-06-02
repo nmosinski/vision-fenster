462acec0b0b4833cc4602e3256bd56a8150c6614
nmosinski <norbert.mosinski@fh-muenster.de>
2020-06-02 08:48:10 +0200

    Basic implementation of the class ProductOption

--- public/src/main/feature/product/model/ProductOption.js
+++ public/src/main/feature/product/model/ProductOption.js
@@ -1,9 +1,44 @@
 const PATH = "public/src/main/feature/product/model/ProductOption.js";
 
-class ProductOption
-{
-	constructor()
-	{
+import AbstractEntity from "public/src/main/common/AbstractEntity.js"
 
+export default class ProductOption extends AbstractEntity
+{
+	constructor(id, productModelId, type, variants)
+	{
+		super(id);
+		this.productModelId = productModelId;
+		this.type = type;
+		this.variants = variants;
+	}
+
+	get productModelId()
+	{
+		return this._productModelId;
+	}
+
+	get type()
+	{
+		return this._type;
+	}
+
+	get variants()
+	{
+		return this._variants;
+	}
+
+	set productModelId(id)
+	{
+		this._productModelId = id;
+	}
+
+	set type(type)
+	{
+		this._type = type;
+	}
+
+	set variants(variants)
+	{
+		this._variants = variants;
 	}
 }
