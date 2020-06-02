d476255557f68e6c92d4365ef2619fc18c5e0b4c
nmosinski <norbert.mosinski@fh-muenster.de>
2020-06-02 08:56:26 +0200

    Basic implementation of AbstractEntity.

--- public/src/main/common/AbstractEntity.js
+++ public/src/main/common/AbstractEntity.js
@@ -1,9 +1,19 @@
 const PATH = "public/src/main/common/AbstractEntity.js";
 
-class AbstractEntity
+export default class AbstractEntity
 {
-	constructor()
+	constructor(id)
 	{
+		this.id = id;
+	}
 
+	get id()
+	{
+		return this._id;
+	}
+
+	set id(id)
+	{
+		this._id = id;
 	}
 }
