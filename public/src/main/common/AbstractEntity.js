cd5fdfb6b195e6d1c90a2fd0102b7797b80a099e
nmosinski <norbert.mosinski@fh-muenster.de>
2020-06-10 13:26:48 +0200

    Added documentation.

--- public/src/main/common/AbstractEntity.js
+++ public/src/main/common/AbstractEntity.js
@@ -1,17 +1,33 @@
 const PATH = "public/src/main/common/AbstractEntity.js";
 
+/**
+ * @class
+ * A class representing an abstract entity.
+ */
 export default class AbstractEntity
 {
+	/**
+	 * Create AbstractEntity.
+	 * @param {string} id - The id of this entity. 
+	 */
 	constructor(id)
 	{
 		this.id = id;
 	}
 
+	/**
+	 * Get id.
+	 * @return {string} The id of this entity.
+	 */
 	get id()
 	{
 		return this._id;
 	}
 
+	/**
+	 * Set id.
+	 * @param {string} id - The id of this entity.
+	 */
 	set id(id)
 	{
 		this._id = id;
