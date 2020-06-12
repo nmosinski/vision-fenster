409fc1c861045b5e6a04b8bb52f1886d537e2c95
nmosinski <norbert.mosinski@fh-muenster.de>
2020-06-12 16:53:07 +0200

    Update and new repository changes

---
+++ public/src/main/feature/product/infrastructure/data/wixData/WixDataProductOptionTypeRepository.js
@@ -0,0 +1,76 @@
+const PATH = "public/src/main/feature/product/infrastructure/data/wixData/WixDataProductOptionTypeRepository.js";
+
+import ProductOptionType from "public/src/main/feature/product/model/ProductOptionType.js"
+import IProductOptionTypeRepository from "public/src/main/feature/product/model/IProductOptionTypeRepository.js"
+import WixDataRepository from "public/src/main/common/wixData/WixDataRepository.js"
+import KVMap from "public/src/main/common/util/map/KVMap.js"
+import List from "public/src/main/common/util/list/List.js"
+
+import JsTypes from "public/src/main/common/util/jsTypes/JsTypes.js"
+
+let COLLECTION_NAME = "product_option_type"
+let MAPPING = {"id":"_id", "productModelId":"productModelId", "title": "title"};
+
+class WixDataProductOptionTypeRepository extends IProductOptionTypeRepository(WixDataRepository)
+{
+	constructor(authorisation=true, hooks=true)
+	{
+        super(COLLECTION_NAME, new KVMap(MAPPING), authorisation, hooks);
+	}
+
+	/**
+	 * @override
+	 * @inheritDoc
+	 */
+	async getProductOptionType(productOptionTypeId)
+	{
+		return this.get(productOptionTypeId).then((v) => {
+                return new ProductOptionType(v.id, v.productOptionModelId, v.title);	
+		});
+	}
+
+	/**
+	 * @override
+	 * @inheritDoc
+	 */
+	async getProductOptionTypesByProductModelId(productModelId)
+	{
+		let query = this.query().eq("productModelId", productModelId);
+		let objects = await this.find(query);
+		
+		let variants = new List();
+		for(let idx in objects)
+			variants.add(new ProductOptionType(objects[idx].id, objects[idx].productModelId, objects[idx].title));
+
+		return variants;
+	}
+
+	/**
+	 * @override
+	 * @inheritDoc
+	 */
+	async saveProductOptionType(productOptionType)
+	{
+		await this.insert(productOptionType);
+	}
+
+	/**
+	 * @override
+	 * @inheritDoc
+	 */
+	async updateProductOptionType(productOptionType)
+	{
+		await this.update(productOptionType);
+	}
+
+	/**
+	 * @override
+	 * @inheritDoc
+	 */
+	async deleteProductOptionType(productOptionTypeId)
+	{
+		await this.remove(productOptionTypeId);
+	}
+}
+
+export default WixDataProductOptionTypeRepository
--- public/src/main/feature/product/infrastructure/data/wixData/WixDataProductOptionVariantRepository.js
+++ public/src/main/feature/product/infrastructure/data/wixData/WixDataProductOptionVariantRepository.js
@@ -3,13 +3,14 @@ const PATH = "public/src/main/feature/product/infrastructure/data/wixData/WixDat
 import ProductOptionVariant from "public/src/main/feature/product/model/ProductOptionVariant.js"
 import IProductOptionVariantRepository from "public/src/main/feature/product/model/IProductOptionVariantRepository.js"
 import KVMap from "public/src/main/common/util/map/KVMap.js"
+import List from "public/src/main/common/util/list/List.js"
 import WixDataRepository from "public/src/main/common/wixData/WixDataRepository.js"
 
 import JsTypes from "public/src/main/common/util/jsTypes/JsTypes.js"
 
 let VARIANT_COLLECTION_NAME = "product_option_variant"
 let IMAGE_COLLECTION_NAME = "product_option_variant_image"
-let VARIANT_MAPPING = {"id":"_id", "productOptionId":"productOptionId", "title": "title"};
+let VARIANT_MAPPING = {"id":"_id", "productOptionTypeId":"productOptionTypeId", "title": "title"};
 let IMAGE_MAPPING = {"id":"_id", "productOptionVariantId":"productOptionVariantId", "title": "title", "image": "image"};
 
 class WixDataProductOptionVariantRepository extends IProductOptionVariantRepository(WixDataRepository)
@@ -31,6 +32,30 @@ class WixDataProductOptionVariantRepository extends IProductOptionVariantReposit
                 return new ProductOptionVariant(v.id, v.productOptionId, v.title, image);
 			});	
 		});
+	}
+
+	/**
+	 * @override
+	 * @inheritDoc
+	 */
+	async getProductOptionVariantsByProductOptionTypeId(productOptionTypeId)
+	{
+		let query = this.query().eq("productOptionTypeId", productOptionTypeId);
+		let objects = await this.find(query);
+		if(JsTypes.isEmpty(objects))
+			return null;
+			
+		let variants = new List();
+		for(let idx in objects)
+			variants.add(new ProductOptionVariant(objects[idx].id, objects[idx].productOptionTypeId, objects[idx].title));
+
+		let tmpVariantArr = variants.toArray();
+		for(let idx in tmpVariantArr)
+		{
+			let image = await this._wixDataImageRepository.getImage(tmpVariantArr[idx].id);
+			tmpVariantArr[idx].image = image;
+		}
+		return variants;
 	}
 
 	/**
