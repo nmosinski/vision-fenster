c311c1eaf00d62dcd76232850ea59e221e33b2d1
nmosinski <mosinski.norbert+admin@gmail.com>
2020-07-04 14:26:05 +0200

    Added comments in RoleModel and added missing abstract methods

--- public/src/main/common/RoleModel.ts
+++ public/src/main/common/RoleModel.ts
@@ -1,10 +1,19 @@
 import AbstractModel from "./AbstractModel";
 import IComparable from "./util/IComparable";
 
+/**
+ * @class
+ * A class representing a dummy model for accessing the role table of two other models.
+ */
 class RoleModel extends AbstractModel<RoleModel> implements IComparable
 {
     private _model1: AbstractModel<any>;
     private _model2: AbstractModel<any>;
+    /**
+     * Create a dummy RoleModel for accessing the role table of the given two models.
+     * @param {AbstractModel<any>} model1 A model.
+     * @param {AbstractModel<any>} model2 Another model.
+     */
     constructor(model1: AbstractModel<any>, model2: AbstractModel<any>)
     {
         super();
@@ -27,6 +36,11 @@ class RoleModel extends AbstractModel<RoleModel> implements IComparable
         return false;
     }
 
+    /**
+     * Check if this role model refers to the given model.
+     * @param {AbstractModel<any>} model The model this RoleModel is maybe referring to.
+     * @return {boolean} True if this model refers to the given model, else false. 
+     */
     hasReference(model: AbstractModel<any>): boolean
     {
         if(this.model1.constructor === model.constructor && this.model1.pk === model.pk)
@@ -36,36 +50,65 @@ class RoleModel extends AbstractModel<RoleModel> implements IComparable
         return false;
     }
 
+    /**
+     * @override
+     * @inheritdoc
+     */
     newInstance(): RoleModel 
     {
         return new RoleModel(null, null);
     }
 
     /**
+     * @overrride
+     * @inheritdoc
+     */
+    addRelations(): void 
+    {
+        // Nothing to add.
+    }
+
+    /**
      * @override
      * @inheritdoc
      */
-    get tableName()
+    get tableName(): string
     {
         return AbstractModel.roleTableNameOf(this.model1.tableName, this.model2.tableName);
     }
 
-    get model1()
+    /**
+     * Get the first model this RoleTable refers to.
+     * @returns {AbstractModel<any>} The first model this RoleModel refers to.
+     */
+    get model1(): AbstractModel<any>
     {
         return this._model1;
     }
 
-    get model2()
+    /**
+     * Get the second model this RoleTable refers to.
+     * @returns {AbstractModel<any>} The second model this RoleModel refers to.
+     */
+    get model2(): AbstractModel<any>
     {
         return this._model2;
     }
 
-    set model1(model)
+    /**
+     * Set the first model this RoleTable refers to.
+     * @param {AbstractModel<any>} model The first model this RoleModel refers to.
+     */
+    set model1(model: AbstractModel<any>)
     {
         this._model1 = model;
     }
 
-    set model2(model)
+    /**
+     * Set the second model this RoleTable refers to.
+     * @param {AbstractModel<any>} model The second model this RoleModel refers to.
+     */
+    set model2(model: AbstractModel<any>)
     {
         this._model2 = model;
     }
