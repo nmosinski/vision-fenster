9c9ed757a2eff22e8fdb9059fa2f27478606921f
nmosinski <mosinski.norbert+admin@gmail.com>
2020-07-26 22:14:33 +0200

    ProductDefinitionParsingService returns a map now

--- public/main/feature/product/service/ProductDefinitionParsingService.ts
+++ public/main/feature/product/service/ProductDefinitionParsingService.ts
@@ -3,43 +3,37 @@ import List from "../../../common/util/collections/list/List";
 import ProductOptionDefinition from "../model/ProductOptionDefinition";
 import Combination from "../model/Combination";
 import CombinationRequirement from "../model/CombinationRequirement";
+import KVMap from "../../../common/util/collections/map/KVMap";
 
-class ProductDefinitionParsingService
-{
-    static parseFromJson(json: object): List<ProductDefinition>
-    {
+class ProductDefinitionParsingService {
+    static parseFromJson(json: object): KVMap<string, ProductDefinition> {
         return ProductDefinitionParsingService.parse(json);
     }
 
-    private static parse(productDefObj: object): List<ProductDefinition>
-    {
-        let productDefinitions = new List<ProductDefinition>();
-        for(var key1 in productDefObj)
-        {   
+    private static parse(productDefObj: object): KVMap<string, ProductDefinition> {
+        let productDefinitions = new KVMap<string, ProductDefinition>();
+        for (var key1 in productDefObj) {
             let productDef = productDefObj[key1];
             let productDefinition = new ProductDefinition(productDef.productModel);
-            
-            for(var key2 in productDef.productOptionDefinitions)
-            {
+
+            for (var key2 in productDef.productOptionDefinitions) {
                 let productOptDef = productDef.productOptionDefinitions[key2];
                 let productOptionDefinition = new ProductOptionDefinition(productOptDef.type, productOptDef.required);
 
-                for(var key3 in productOptDef.combinations)
-                {
+                for (var key3 in productOptDef.combinations) {
                     let comb = productOptDef.combinations[key3];
                     let combination = new Combination();
 
-                    for(var key4 in comb.tags)
+                    for (var key4 in comb.tags)
                         combination.addTag(comb.tags[key4]);
-                    
-                    for(var key5 in comb.requirements)
-                    {
+
+                    for (var key5 in comb.requirements) {
                         let combReq = comb.requirements[key5];
                         let combinationRequirement = new CombinationRequirement(combReq.productOptionType);
 
-                        for(var key6 in combReq.tags)
+                        for (var key6 in combReq.tags)
                             combinationRequirement.addTag(combReq.tags[key6]);
-                        
+
                         combination.setRequirement(combinationRequirement);
                     }
 
@@ -49,7 +43,7 @@ class ProductDefinitionParsingService
                 productDefinition.setProductOptionDefinition(productOptionDefinition);
             }
 
-            productDefinitions.add(productDefinition);
+            productDefinitions.add(productDefinition.model, productDefinition);
         }
 
         return productDefinitions;
