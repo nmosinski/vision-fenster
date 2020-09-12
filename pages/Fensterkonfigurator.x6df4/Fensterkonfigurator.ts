e89e400d083de9766da5ac6829726dcb046e4679
nmosinski <mosinski.norbert+admin@gmail.com>
2020-09-12 15:31:40 +0200

    cs + typing

--- pages/Fensterkonfigurator.x6df4/Fensterkonfigurator.ts
+++ pages/Fensterkonfigurator.x6df4/Fensterkonfigurator.ts
@@ -6,23 +6,17 @@ import Product from "../../public/main/feature/product/model/Product";
 import AbstractProductConfigurationService from "../../public/main/feature/product/service/configurator/AbstractProductConfigurationService";
 import ProductConfigurationServiceFactory from "../../public/main/feature/product/factory/ProductConfigurationServiceFactory";
 import List from "../../public/main/common/util/collections/list/List";
-//import ProductController from "../../backend/main/feature/product/controllers/ProductController";
-//@ts-ignore
+// @ts-ignore
 import { index } from "backend/main/feature/product/controllers/ProductController.jsw";
-import QueryResult from "../../public/main/common/orm/QueryResult";
 import Tag from "../../public/main/feature/product/model/Tag";
-import { Query } from "../../public/main/common/orm/WixDatabase";
-import safeStringify from "../../public/main/common/util/jsTypes/safeStringify";
 
-var productModel: ProductModel;
-var product: Product;
-var productConfiguationService: AbstractProductConfigurationService;
-var allProductOptions: List<ProductOption>;
-let cache = [];
+let productModel: ProductModel;
+let product: Product;
+let productConfiguationService: AbstractProductConfigurationService;
+let allProductOptions: List<ProductOption>;
 
-
-//@ts-ignore
-$w.onReady(async function () {
+// @ts-ignore
+$w.onReady(async () => {
 
 	productModel = (await ProductModel.find(ProductModel)).first();
 	await productModel.loadChain([ProductOptionType, ProductOption, Tag]);
@@ -30,17 +24,21 @@ $w.onReady(async function () {
 
 	allProductOptions = new List<ProductOption>();
 	productModel.productOptionTypes.foreach(types => allProductOptions = allProductOptions.OR(types.productOptions));
+
 	console.log(allProductOptions);
+
 	productConfiguationService = ProductConfigurationServiceFactory.byModel(productModel);
 	product = new Product();
 
 	productConfiguationService.fillMissingProductOptionsWithDefault(allProductOptions, product);
+
 	console.log(product);
 
 	initRepeater();
 	applyFilterForRepeater();
 	displayProductAsActualConfiguration();
-	//product.productOptions.foreach(option => updateViewSelectedItems(option.productOptionType.title));
+
+	// product.productOptions.foreach(option => updateViewSelectedItems(option.productOptionType.title));
 });
 
 function onProductOptionSelection(productOption: ProductOption) {
@@ -50,34 +48,34 @@ function onProductOptionSelection(productOption: ProductOption) {
 }
 
 function displayProductAsActualConfiguration(): void {
-	//@ts-ignore
+	// @ts-ignore
 	$w("#imageConfiguration").src = (product.hasOption('profil')) ? product.getOption("profil").image : 'https://wuerdest-du-eher.de/ratgeber/wp-content/uploads/2017/07/was-heisst-nope-auf-deutsch.jpg';
-	//@ts-ignore
+	// @ts-ignore
 	$w("#textConfigurationPrice").text = "" + product.price + "€";
 
-	//@ts-ignore
+	// @ts-ignore
 	$w("#repeaterConfigurationDetails").data = product.productOptions.toArray();
 }
 
-function repeaterConfigurationDetailsOnItemReadyFunction($item: any, productOption: ProductOption, index: number): void {
+function repeaterConfigurationDetailsOnItemReadyFunction($item: any, productOption: ProductOption): void {
 	$item("#textConfigurationDetails").text = productOption.productOptionType.title + ": " + productOption.value;
 }
 
 function updateViewSelectedItems(productOptionTypeTitle: string): void {
-	repeaterByProductOptionTypeTitle(productOptionTypeTitle).forEachItem(($item: any, productOption: ProductOption, index: number) => {
-		if (product.getOption(productOptionTypeTitle).id === productOption["_id"])
+	repeaterByProductOptionTypeTitle(productOptionTypeTitle).forEachItem(($item: any, productOption: ProductOption) => {
+		if (product.getOption(productOptionTypeTitle).id === productOption.id)
 			$item("#vectorImage" + productOptionTypeTitleAsGuiElementName(productOptionTypeTitle)).show();
 		else
 			$item("#vectorImage" + productOptionTypeTitleAsGuiElementName(productOptionTypeTitle)).hide();
 	});
 }
 
-function getDefaultOnItemReadyRepeaterFunction(optionTypeTitle: string): Function {
-	return ($item, productOption: ProductOption, index: number) => {
+function getDefaultOnItemReadyRepeaterFunction(optionTypeTitle: string): (item: any, productOption: ProductOption) => void {
+	return ($item, productOption: ProductOption) => {
 		$item("#image" + optionTypeTitle).src = productOption.image;
 		$item("#vectorImage" + productOptionTypeTitleAsGuiElementName(optionTypeTitle)).hide();
 		$item("#text" + productOptionTypeTitleAsGuiElementName(optionTypeTitle)).text = productOption.productOptionType.title;
-		$item("#image" + optionTypeTitle).onClick((event) => {
+		$item("#image" + optionTypeTitle).onClick((event: any) => {
 			onProductOptionSelection(productOption);
 			updateViewSelectedItems(productOption.productOptionType.title);
 		});
@@ -88,13 +86,13 @@ function productOptionTypeTitleAsGuiElementName(productOptionTypeTitle: string):
 	return JsString.capitalizeFirstLetter(JsString.replaceGermanSpecialLetters(productOptionTypeTitle));
 }
 function repeaterByProductOptionTypeTitle(productOptionTypeTitle: string): any {
-	//@ts-ignore
+	// @ts-ignore
 	return $w("#repeater" + productOptionTypeTitleAsGuiElementName(productOptionTypeTitle));
 }
 
 function applyFilterForRepeater(): void {
 	productModel.productOptionTypes.foreach((productOptionType) => {
-		let filteredData = productConfiguationService.filterValidOptions(allProductOptions, product, productOptionType.title);
+		const filteredData = productConfiguationService.filterValidOptions(allProductOptions, product, productOptionType.title);
 		repeaterByProductOptionTypeTitle(productOptionType.title).data = filteredData.toArray();
 
 	});
@@ -102,13 +100,13 @@ function applyFilterForRepeater(): void {
 
 function initRepeater(): void {
 	productModel.productOptionTypes.foreach((productOptionType) => {
-		let optionTypeTitle = productOptionTypeTitleAsGuiElementName(productOptionType.title);
-		let onItemReadyFunction = getDefaultOnItemReadyRepeaterFunction(optionTypeTitle);
+		const optionTypeTitle = productOptionTypeTitleAsGuiElementName(productOptionType.title);
+		const onItemReadyFunction = getDefaultOnItemReadyRepeaterFunction(optionTypeTitle);
 		repeaterByProductOptionTypeTitle(optionTypeTitle).onItemReady(onItemReadyFunction);
 		repeaterByProductOptionTypeTitle(optionTypeTitle).data = productOptionType.productOptions.toArray();
 	});
 
-	//@ts-ignore
+	// @ts-ignore
 	$w("#repeaterConfigurationDetails").onItemReady(repeaterConfigurationDetailsOnItemReadyFunction);
 }
 
